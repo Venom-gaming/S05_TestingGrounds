306ec21f141bb39329377b001e1bc8032141e00f
Nathaniel-PC\Nathaniel <nrvrana16@gmail.com>
2017-10-29 20:04:50 -0700

    Section 5 Lecture 223

---
+++ Source/P05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrolRoute.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+
+
+
+	//Get patrol route
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; };
+
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if(PatrolPoints.Num() == 0)
+	{ 
+		UE_LOG(LogTemp, Warning, TEXT("A Guard is Missing Patrol Points!"))
+		return EBTNodeResult::Failed;
+	}
+
+	//Set Next Waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle Index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+
+	
+	return EBTNodeResult::Succeeded;
+}
